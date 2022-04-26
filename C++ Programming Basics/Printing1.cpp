fc253419ce067c19a0ea75666b30c902d526c690
Saugatchaudhary <54131741+Saugatchaudhary@users.noreply.github.com>
2022-04-26 20:20:45 +0545

    Create Printing1.cpp

---
+++ C++ Programming Basics/Printing1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>//outpout data in the screen
+using namespace std;
+int main(){//begins execution
+  cout << "hello" endl; //First line ends
+  dout << "Panda";
+  return 0;//end task sucessfully
+}
