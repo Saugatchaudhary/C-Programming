fb70f3b18ab4f0a26c3b066f4f429afef40d6c35
Saugatchaudhary <54131741+Saugatchaudhary@users.noreply.github.com>
2022-04-26 21:06:08 +0545

    Create DataType.cpp

---
+++ C++ Programming Basics/DataType.cpp
@@ -0,0 +1,20 @@
+/*
+Data Types
+
+int = Use to store numbers [Long, Short] eg :- 1, 2, 3 etc.
+float = Use to store number with decimal eg :- 1.2, 1.3, 1.4 etc.
+double = Bigger then float
+boolean or bool = True or False
+Void = Empty
+
+*/
+#include <iostream>
+using namespace std;
+int main(){
+  int a = 10;
+  float b = 20.0;
+  double c = 30.5;
+  bool d = False;
+  cout << "The value of a is"<< a;
+  return 0;
+}
