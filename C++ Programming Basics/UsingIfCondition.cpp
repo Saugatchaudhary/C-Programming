f3239d5c580e6a3461d8ead3f8fd4031774c70a8
Saugatchaudhary <54131741+Saugatchaudhary@users.noreply.github.com>
2022-04-28 13:10:30 +0545

    Update UsingIfCondition.cpp

--- C++ Programming Basics/UsingIfCondition.cpp
+++ C++ Programming Basics/UsingIfCondition.cpp
@@ -19,8 +19,11 @@ int main(){
   int age;
   cout<<"Enter your age to check eligibility: ";
   cin>>age;
-  if(age>=18){
+  if(age>=18 && age<=120){
     cout<<"You are eligible"<<endl;
+  }
+  else if(age>=121){
+    cout<<"You are dead so cannot vote"endl;
   }
   else{
     cout<<"You are not eligible"<<endl;
