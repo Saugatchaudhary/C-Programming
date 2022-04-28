dc0eadac04d9282b1314773f6a1e3741120acfb6
Saugatchaudhary <54131741+Saugatchaudhary@users.noreply.github.com>
2022-04-28 16:29:46 +0545

    Update BreakAndContinue.cpp

--- C++ Programming Basics/BreakAndContinue.cpp
+++ C++ Programming Basics/BreakAndContinue.cpp
@@ -5,4 +5,57 @@ Break and Continue
 Break : Terminate from loop or if condition or while
 Continue : Bypass
 
+Syntax :
+
+//Break syntax
+for (inatilizer; condition; increasing/Decreasing){
+    if(condition){
+        break;
+    }
+}
+//Continue syntax
+for (inatilizer; condition; increasing/Decreasing){
+    if(condition){
+        continue;
+    }
+}
+
 */
+/*
+//Write a program to ascending numbers and use break.
+#include <iostream>
+using manespace std;
+int main(){
+  int a=5;
+  for(int i=1; i<=a; i++){
+    if(i==3){
+      break;
+    }
+    cout<<"output is: "<<i<<endl;
+  }
+  return 0;
+}
+*/
+
+
+//Write a program to print descending number using continue.
+#include <iostream>
+using namespace std;
+int main(){
+  for(int i = 5; i>=1; i--){
+    if(i == 3){
+      continue;
+    }
+    cout<<"Output is: "<<i<<endl;
+  }
+  return 0;
+}
+
+
+
+
+
+
+
+
+
